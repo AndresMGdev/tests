6dea8f42884bbe6467e39dcb11d785c8fd45f4af
AndresMGdev <cmendoza022002@gmail.com>
2023-09-09 19:01:16 -0500

    Pages and dinamic routing

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from 'astro:content';
+
+// 2. Define a `type` and `schema` for each collection
+const postsCollection = defineCollection({
+  type: 'content', // v2.5.0 and later
+  schema: ({image}) => z.object({
+    title: z.string(),
+    date: z.string(),
+    image: image(),
+    author: z.string()
+  }),
+});
+
+// 3. Export a single `collections` object to register your collection(s)
+export const collections = {
+  posts: postsCollection,
+};
